46dddcc7173e3691f66865bbf645e4f144235078
Coder-yt <animeupdatesau04@gmail.com>
2026-05-23 23:25:31 +0530

    config.py

--- config.py
+++ config.py
@@ -1,15 +1,42 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
 import os
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
 MONGO_URI = os.getenv("MONGO_URI")
 
 BOT_USERNAME = os.getenv("BOT_USERNAME")  # without @
+
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
+
 OWNER_ID = os.getenv("OWNER_ID")
 if OWNER_ID:
     OWNER_ID = int(OWNER_ID)
 else:
     raise Exception("OWNER_ID is not set in environment variables")
 PORT = int(os.getenv("PORT", 10000))
+
+# ------------------------- #
+# Don't Remove Credit 
+# Ask Doubt @AU_Bot_Discussion 
+# Owner @Mr_Mohammed_29 
+# ------------------------- #
