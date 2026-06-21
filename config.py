f4580b7fd7e2f2d5d5dfbc0e676b6519f65fecad
MohammedDev-yt <animeworldday2005@gmail.com>
2026-06-21 18:40:51 +0530

    config.py

--- config.py
+++ config.py
@@ -1,42 +1,52 @@
+
 # ------------------------- #
 # Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Owner @Mr_Mohammed_29
 # ------------------------- #
 
 import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+def must_get(name):
+    value = os.getenv(name)
+    if not value:
+        raise Exception(f"{name} is not set in environment variables")
+    return value
 
 # ------------------------- #
 # Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Owner @Mr_Mohammed_29
 # ------------------------- #
 
-MONGO_URI = os.getenv("MONGO_URI")
-
-BOT_USERNAME = os.getenv("BOT_USERNAME")  # without @
-
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+API_ID = int(must_get("API_ID"))
+API_HASH = must_get("API_HASH")
+BOT_TOKEN = must_get("BOT_TOKEN")
 
 # ------------------------- #
 # Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Owner @Mr_Mohammed_29
 # ------------------------- #
 
-OWNER_ID = os.getenv("OWNER_ID")
-if OWNER_ID:
-    OWNER_ID = int(OWNER_ID)
-else:
-    raise Exception("OWNER_ID is not set in environment variables")
-PORT = int(os.getenv("PORT", 10000))
+MONGO_URI = must_get("MONGO_URI")
 
 # ------------------------- #
 # Don't Remove Credit 
-# Ask Doubt @AU_Bot_Discussion 
-# Owner @Mr_Mohammed_29 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+BOT_USERNAME = must_get("BOT_USERNAME")
+
+CHANNEL_ID = int(must_get("CHANNEL_ID"))
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+OWNER_ID = int(must_get("OWNER_ID"))
+
+PORT = int(os.getenv("PORT", "10000"))
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
 # ------------------------- #
